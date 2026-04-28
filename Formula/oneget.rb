2d0b58df8bc92ea657ff0b9946c20e37919556cd
mudasarfarooq <mudassar.farooq@futuredevsolutions.com>
2026-04-28 11:44:45 +0500

    fixs

--- Formula/oneget.rb
+++ Formula/oneget.rb
@@ -1,25 +1,29 @@
 class Oneget < Formula
   desc "CLI for 1get link shortening service"
-  homepage "https://github.com/FutureDevSolutions/1get-cli"
+  homepage "https://github.com/1get/cli"
   version "0.1.3"
   license "MIT"
+
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/FutureDevSolutions/1get-cli/releases/download/v0.1.2/1get-0.1.3-macos-aarch64.tar.gz"
-      sha256 "REPLACE_WITH_MACOS_ARM64_SHA"
+      url "https://github.com/1get/cli/releases/download/v0.1.3/1get-0.1.3-macos-aarch64.tar.gz"
+      sha256 "PASTE_MACOS_ARM64_SHA"
     else
-      url "https://github.com/FutureDevSolutions/1get-cli/releases/download/v0.1.2/1get-0.1.3-macos-x86_64.tar.gz"
-      sha256 "REPLACE_WITH_MACOS_X64_SHA"
+      url "https://github.com/1get/cli/releases/download/v0.1.3/1get-0.1.3-macos-x86_64.tar.gz"
+      sha256 "PASTE_MACOS_X64_SHA"
     end
   end
+
   on_linux do
-    url "https://github.com/FutureDevSolutions/1get-cli/releases/download/v0.1.2/1get-0.1.3-linux-x86_64.tar.gz"
-    sha256 "REPLACE_WITH_LINUX_X64_SHA"
+    url "https://github.com/1get/cli/releases/download/v0.1.3/1get-0.1.3-linux-x86_64.tar.gz"
+    sha256 "PASTE_LINUX_X64_SHA"
   end
+
   def install
     bin.install "1get"
     bin.install_symlink "1get" => "oneget"
   end
+
   test do
     assert_match version.to_s, shell_output("#{bin}/1get --version")
   end
