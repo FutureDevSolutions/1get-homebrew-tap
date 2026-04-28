1e335fee8e48c72a52a43de3b94bb03d0ae7b222
mudasarfarooq <mudassar.farooq@futuredevsolutions.com>
2026-04-28 11:48:56 +0500

    version set

--- Formula/oneget.rb
+++ Formula/oneget.rb
@@ -1,21 +1,21 @@
 class Oneget < Formula
   desc "CLI for 1get link shortening service"
-  homepage "https://github.com/1get/cli"
-  version "0.1.3"
+  homepage "https://github.com/FutureDevSolutions/1get-cli"
+  version "0.1.7"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/1get/cli/releases/download/v0.1.3/1get-0.1.3-macos-aarch64.tar.gz"
+      url "https://github.com/FutureDevSolutions/1get-cli/releases/download/v0.1.7/1get-0.1.7-macos-aarch64.tar.gz"
       sha256 "PASTE_MACOS_ARM64_SHA"
     else
-      url "https://github.com/1get/cli/releases/download/v0.1.3/1get-0.1.3-macos-x86_64.tar.gz"
+      url "https://github.com/FutureDevSolutions/1get-cli/releases/download/v0.1.7/1get-0.1.7-macos-x86_64.tar.gz"
       sha256 "PASTE_MACOS_X64_SHA"
     end
   end
 
   on_linux do
-    url "https://github.com/1get/cli/releases/download/v0.1.3/1get-0.1.3-linux-x86_64.tar.gz"
+    url "https://github.com/FutureDevSolutions/1get-cli/releases/download/v0.1.7/1get-0.1.7-linux-x86_64.tar.gz"
     sha256 "PASTE_LINUX_X64_SHA"
   end
 
